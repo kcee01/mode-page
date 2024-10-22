5af352863205ba216c725d7cb556f6e05f3113c3
Cliff Keabetswe <105539368+kcee01@users.noreply.github.com>
2024-10-22 13:43:02 +0200

    update logout.html and add logout.js

---
+++ logout.js
@@ -0,0 +1,20 @@
+
+// Configure AWS Cognito
+const poolData = {
+    UserPoolId: 'us-east-1_s0CLhJKA9', // Your user pool id here
+    ClientId: '7idcfgsp88dmqgnum16bmapjgg' // Your client id here
+};
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+// Logout function using class selector
+document.querySelector('.logout-button').addEventListener('click', function() {
+    const cognitoUser = userPool.getCurrentUser();
+
+    if (cognitoUser) {
+        cognitoUser.signOut();
+        alert("You have been logged out successfully.");
+        window.location.href = 'login.html'; // Redirect to login page or home page
+    } else {
+        alert("No user is currently logged in.");
+    }
+});
