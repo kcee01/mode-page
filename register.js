7b18f99c5bcfd7a9d3937aa686e64a6eb3c66a50
Cliff Keabetswe <105539368+kcee01@users.noreply.github.com>
2024-10-22 11:18:19 +0200

    add custom:role

--- register.js
+++ register.js
@@ -46,7 +46,7 @@ document.getElementById('register-button').addEventListener('click', async () =>
             UserAttributes: [
                 { Name: 'email', Value: email },
                 { Name: 'name', Value: name },
-                { Name: 'Updated at (attribute)', Value: selectedRole }, // Add role as a custom attribute
+                { Name: 'custom:role', Value: selectedRole }, // Add role as a custom attribute
             ],
         };
 
