9bf02d6e5e7035ba9e34a89716a6c47de62d53ea
Cliff Keabetswe <105539368+kcee01@users.noreply.github.com>
2024-11-01 00:23:37 +0200

    up2

--- verifyC.js
+++ verifyC.js
@@ -14,7 +14,6 @@
                 body: JSON.stringify(jsonData)
             });
             const result = await response.json();
-            //console.log(result);
             document.getElementById('verification-notification').innerText = result.message;
         } catch (error) {
             console.error('Error:', error);
