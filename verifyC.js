54b6ba5e1cf1c23d90368c61e46a4b8fc215d248
Cliff Keabetswe <105539368+kcee01@users.noreply.github.com>
2024-10-31 23:39:44 +0200

    api update1

--- verifyC.js
+++ verifyC.js
@@ -6,14 +6,13 @@
         const jsonData = Object.fromEntries(formData.entries());
 
         try {
-            const response = await fetch('https://your-api-id.execute-api.region.amazonaws.com/dev/verify-consumer', { // Replace with your actual API Gateway URL
+            const response = await fetch('https://1f6atxwy1d.execute-api.us-east-1.amazonaws.com/dev/Verify_Consumers_Function', { // Replace with your actual API Gateway URL
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(jsonData)
             });
-
 
             const result = await response.json();
             document.getElementById('verification-notification').innerText = result.message;
