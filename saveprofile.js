93d5d7431dd77a04cef5694ae4a6830492ee2360
Cliff Keabetswe <105539368+kcee01@users.noreply.github.com>
2024-11-02 15:33:13 +0200

    recent update

--- saveprofile.js
+++ saveprofile.js
@@ -8,7 +8,7 @@ async function saveAdminProfile() {
     };
 
     try {
-        const response = await fetch('https://oypjcbf6pk.execute-api.us-east-1.amazonaws.com/dev/get_admin_function', {
+        const response = await fetch('https://yiykq4m8n3.execute-api.us-east-1.amazonaws.com/prod/save_profile_function', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
