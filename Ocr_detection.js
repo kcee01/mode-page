0d24c5f56e11f67a69339cb65bc907934c2b9c9d
Cliff Keabetswe <105539368+kcee01@users.noreply.github.com>
2024-11-13 00:44:54 +0200

    update2

--- Ocr_detection.js
+++ Ocr_detection.js
@@ -88,7 +88,7 @@ async function submitManualReading() {
     }
 
     try {
-        const response = await fetch("https://n9krhb40p7.execute-api.us-east-1.amazonaws.com/prod/GET_meter_reading_function", {
+        const response = await fetch("https://8klqov49cl.execute-api.us-east-1.amazonaws.com/prod/Manual_meter_reading_function", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
